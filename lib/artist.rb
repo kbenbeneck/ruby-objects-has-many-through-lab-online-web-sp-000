1bdc1886eff593e9aff570169f291e348355f262
Karl Benbeneck <kbenbeneck@gmail.com>
2019-10-27 21:02:30 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,19 @@
+class Artist
+attr_accessor :name, :genre, 
+@@all = []
+
+  def initialize 
+    @@all << self
+  end
+
+  def new_song
+  end
+
+  def songs
+  end
+
+  def genres
+  end
+
+
+end
