c35bcbee831bc7f8632508c895b4c1d2ee9d9778
Karl Benbeneck <kbenbeneck@gmail.com>
2019-10-28 01:18:24 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -11,4 +11,17 @@ class Doctor
  def self.all
    @@all
  end
+
+ def new_appointment(patient, date)
+   Appointment.new(date, patient, self)
+ end
+
+
+ def appointments
+   Appointment.all.select { |appointment| appointment.doctor == self  }
+ end
+
+ def patients
+   appointments.map(&:patient)
+ end
 end
