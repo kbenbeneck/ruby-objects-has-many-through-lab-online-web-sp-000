909b56dd35622533b967aeb8ce5d74bf64ea34e2
Karl Benbeneck <kbenbeneck@gmail.com>
2019-10-28 00:42:38 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,3 +1,18 @@
 class Genre
+  attr_accessor :name
 
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def songs
+    Songs.all { |song| song.genre == self }
+  end
 end
