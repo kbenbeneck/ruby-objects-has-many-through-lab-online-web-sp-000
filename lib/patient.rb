d95d04ea6773bf92a09a7bb1b2ab9d82a4b8aa7a
Karl Benbeneck <kbenbeneck@gmail.com>
2019-10-28 11:27:49 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -14,7 +14,7 @@ class Patient
 
 
   def appointments
-    Appointment.all.select { |appointment| appointment.doctor == self  }
+    Appointment.all.select { |appointment| appointment.patient == self  }
   end
 
   def doctor
